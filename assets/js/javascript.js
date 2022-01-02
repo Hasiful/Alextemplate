9312d7c95992d64a8913849893aba7f19e0f3fd6
hasiful <hasifulislam32@gmail.com>
2022-01-03 00:16:56 +0600

    first commit

---
+++ assets/js/javascript.js
@@ -0,0 +1,9 @@
+var mixer = mixitup('.protfolio_image_section');
+var mixer = mixitup(containerEl, {
+    selectors: {
+        target: '.blog-item'
+    },
+    animation: {
+        duration: 300
+    }
+});
